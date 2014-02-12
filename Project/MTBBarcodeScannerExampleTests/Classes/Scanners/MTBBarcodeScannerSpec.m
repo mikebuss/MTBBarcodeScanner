f09f3c092e7ff3eda9dfb9815fe1ee8726e0c2c6
Mike Buss <michaeltbuss@gmail.com>
2014-02-12 00:41:38 -0500

    Added Kiwi testing framework

---
+++ Project/MTBBarcodeScannerExampleTests/Classes/Scanners/MTBBarcodeScannerSpec.m
@@ -0,0 +1,19 @@
+//
+//  MTBBarcodeScannerSpec.m
+//  MTBBarcodeScannerExample
+//
+//  Created by Mike Buss on 2/12/14.
+//
+//
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(MTBBarcodeScannerSpec)
+
+describe(@"The barcode scanner", ^{
+    context(@"when scanning barcodes", ^{
+        
+    });
+});
+
+SPEC_END
