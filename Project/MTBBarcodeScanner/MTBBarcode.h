4d628c61bb68a9e5ecd6a4ae6b0ca3f375cb83ca
Hardik <hardik.webatease@gmail.com>
2016-05-06 00:41:39 +0530

    - Carthage support

---
+++ Project/MTBBarcodeScanner/MTBBarcode.h
@@ -0,0 +1,17 @@
+//
+//  MTBBarcodeScanner.h
+//  MTBBarcodeScanner
+//
+//  Created by Hardik on 5/4/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MTBBarcodeScanner.
+FOUNDATION_EXPORT double MTBBarcodeScannerVersionNumber;
+
+//! Project version string for MTBBarcodeScanner.
+FOUNDATION_EXPORT const unsigned char MTBBarcodeScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTBBarcodeScanner/PublicHeader.h>
